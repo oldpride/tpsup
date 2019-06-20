f6c97b2b885d8c13a6ee85d818d926672335477d
Tianhua Han <tianhua_han@yahoo.com>
2019-06-20 18:52:11 -0400

    Initial

--- lib/perl5/TPSUP/LOG.pm
+++ lib/perl5/TPSUP/LOG.pm
@@ -15,6 +15,8 @@ use TPSUP::CSV qw(query_csv2);
 
 my $PatternCfg_by_file_app;
                   
+my $current_patternFile; # a workaround to keep track of which patternFile used. need refactoring
+
 sub parse_PatternFile {
    my ($opt) = @_;
       
@@ -30,15 +32,19 @@ sub parse_PatternFile {
       
    croak "$file is not found" if ! -f $file;
       
+   $current_patternFile = $file;
+
    return if exists $PatternCfg_by_file_app->{$file};
       
-   $PatternCfg_by_file_app->{$file} = query_csv2($file, { ReturnKeyedHash => ['App'],
+   my $result = query_csv2($file, { ReturnKeyedHash => ['App'],
                                                          QuotedInput     => 1,
                                                          NoPrint         => 1,
                                                          # skip comments and blank lines
                                                          MatchPatterns   => ['^[ ]*[^#]'],
                                                          %$opt
                                                });
+
+   $PatternCfg_by_file_app->{$file} = $result->{KeyedHash};
 
    return $PatternCfg_by_file_app->{$file};
 }
@@ -47,7 +53,7 @@ sub get_PatternCfg_by_app {
    my ($app, $opt) = @_;
       
    my $PatternCfg_by_app = parse_PatternFile($opt);
-      
+
    return $PatternCfg_by_app->{$app}->[0];
 }
       
@@ -58,6 +64,7 @@ sub get_PatternCfg {
       
    if ($opt->{App}) {
       $cfg = get_PatternCfg_by_app($opt->{App}, $opt);
+      croak "missing setting for App='$opt->{App}' in $current_patternFile" if ! $cfg;
    }
       
    if ($opt->{TimePattern}) {
@@ -74,7 +81,7 @@ sub get_PatternCfg {
       
    if ($opt->{CheckSettings} && @{$opt->{CheckSettings}}) {
       for my $k (@{$opt->{CheckSettings}}) {
-         croak "missing $k setting" if ! defined($cfg->{$k});
+         croak "missing $k setting in $current_patternFile" if ! defined($cfg->{$k});
          $opt->{verbose} && print STDERR "$k = '$cfg->{$k}'\n";
       }
    }
