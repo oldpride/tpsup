358b9ca1a0e1f6a213d6033fca681c19254b1f99
Tianhua Han <tiahua_han@yahoo.com>
2019-10-23 19:37:46 -0400

    update

--- python3/scripts/edit_mmap.py
+++ python3/scripts/edit_mmap.py
@@ -12,16 +12,28 @@ def main():
     prog = os.path.basename(sys.argv[0])
 
     usage = textwrap.dedent("""
-        use mmap to edit a file.
-        To create a binary test file, use "xxd".
-        echo 0000 0101 6865 6c6c 6f20 776f 726c 640a 000 |xxd -r >edit_mmap.bin
-        note: the first 0000 is the starting address, not the data.
-        To display: xxd edit_mmap.bin
+use mmap to edit a file. Key point: keep the file size the same.   
         """)
 
     examples = textwrap.dedent(f"""
-        examples:
-        {prog} "hello world" "hi space" edit_mmap.bin
+examples:
+    create a test binary file, use "xxd":
+        echo 0000 0101 6865 6c6c 6f20 776f 726c 640a 000 |xxd -r >edit_mmap1.bin   
+    note: the first 0000 is the starting address, not the data.
+        
+    To display
+        xxd edit_mmap1.bin
+
+    Save a copy, so that we can compare later
+        cp edit_mmap1.bin edit_mmap2.bin
+
+    Replace long string with short string
+        {prog} "hello world" "hi space" edit_mmap1.bin
+        diff_binary edit_mmap1.bin edit_mmap2.bin
+
+    Replace short string with long string    
+        {prog} "hi space" "hello world" edit_mmap1.bin
+        diff_binary edit_mmap1.bin edit_mmap2.bin 
         """)
 
     parser = argparse.ArgumentParser(
@@ -48,68 +60,34 @@ def main():
     if args['verbose'] > 0:
         print(f'args =\n{pformat(args)}')
 
-    sys.exit(download_fix(**args))
+    edit_mmap(**args)
+    sys.exit(0)
 
 
 def edit_mmap(**opt):
-    version = opt['version']
-    _dir = opt['dir']
-
-    old_string = opt['old_string']
-    new_string = opt['new_string']
+    old_string = opt['old_string'].encode('utf-8')
+    new_string = opt['new_string'].encode('utf-8')
     filename = opt['filename']
 
     # https://docs.python.org/2/library/mmap.html
     with open(filename, "r+b") as f:
         # memory-map the file, size 0 means whole file
         mm = mmap.mmap(f.fileno(), 0)
-        mm[:] = re.sub('')
-    if not os.path.exists(_dir):
-        os.makedirs(_dir)
-
-    # https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
-    url = f'https://www.onixs.biz/fix-dictionary/{version}/fields_by_tag.html'
-
-    # Connect to the URL
-    response = requests.get(url)
-
-    # Parse HTML and save to BeautifulSoup object¶
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # <p; align = "center" class ="listLinks" > < a href="tagNum_1.html" > 1 < / a > & nbsp; | < a
-    # href="tagNum_2.html" > 2 < / a > & nbsp; | < a href="tagNum_3.html" >
-
-    seen_href = set()
-    download_count = 0
-    for a in soup.findAll('a'):
-        href = a['href']
-        if href.startswith("tagNum_"):
-            if href in seen_href:
-                continue
-            seen_href.add(href)
-
-            local_file = f'{_dir}/{href}'
-
-            break_time = 10
-
-            if os.path.exists(local_file):
-                size = os.stat(local_file).st_size
-                if size > 10000:
-                    print(f'{href} is already downloaded and have a decent size {size} bytes')
-                    continue
-                else:
-                    print(f'{href} is already downloaded and but size is only {size} bytes, try to download again')
-
-            download_url = f'https://www.onixs.biz/fix-dictionary/{version}/{href}'
-            print(f'downloading {download_url} to {local_file}')
-            urllib.request.urlretrieve(download_url, local_file)
-
-            if download_count > 0 and download_count % break_time == 0:
-                time.sleep(2)  # pause the code for a sec so that they won't complain us spamming
+        index = mm.find(old_string)
+        if index != -1:
+            old_len = len(old_string)
+            new_len = len(new_string)
+            if new_len > old_len:
+                sys.stderr.write(f'Warning: new string (len={new_len}) is longer than old string (len={old_len}). '
+                                 f'Please check result\n')
+            mm[index:index+new_len] = new_string
+            if new_len < old_len:
+                mm[index+new_len:index+old_len] = bytearray([0]*(old_len-new_len))
             else:
-                time.sleep(0.1)
-
-            download_count += 1
+                mm[index+new_len] = 0
+        else:
+            sys.stderr.write(f'cannot find old string in {filename}\n')
+        mm.close()
 
 
 if __name__ == '__main__':
