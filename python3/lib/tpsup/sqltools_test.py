d543855c1f7ab670e54932bfc24567a5082519ff
Tianhua Han <tiahua_han@yahoo.com>
2019-11-30 21:24:50 -0500

    update

--- python3/lib/tpsup/sqltools_test.py
+++ python3/lib/tpsup/sqltools_test.py
@@ -8,14 +8,23 @@ import time
 import tpsup.sqltools
 
 
-class TestSqllib(unittest.TestCase):
-    def test_unlock_conn(self, verbose=0):
-        _dir = os.path.dirname(sys.modules["tpsup.sqllib"].__file__)
+class TestSqltools(unittest.TestCase):
+    def test_unlock_conn_oracle(self, verbose=0):
+        _dir = os.path.dirname(sys.modules["tpsup.sqltools"].__file__)
         file = os.path.join(_dir, "sqltools_conn_example.csv")
-        unlocked = tpsup.sqltools.unlock_conn('ora_user@ora_db', connfile=file)
+        conn = tpsup.sqltools.unlock_conn('ora_user@ora_db', connfile=file)
 
-        self.assertEqual(unlocked['string'], 'dbi:Oracle:host=AHOST.abc.com;service_name=AHOST.abc.com;port=1501')
-        self.assertEqual(unlocked['unlocked_password'], 'Hello@123')
+        self.assertEqual(conn.string, 'dbi:Oracle:host=AHOST.abc.com;service_name=AHOST.abc.com;port=1501')
+        self.assertEqual(conn.unlocked_password, 'Hello@123')
+
+    def test_unlock_conn_mssql(self, verbose=0):
+        _dir = os.path.dirname(sys.modules["tpsup.sqltools"].__file__)
+        file = os.path.join(_dir, "sqltools_conn_example.csv")
+        conn = tpsup.sqltools.unlock_conn('sql_user@sql_db', connfile=file)
+
+        self.assertEqual(conn.string,
+                         'dbi:ODBC:DRIVER={ODBC Driver 11 for SQL Server};SERVER=AHOST.abc.com,1502;database=app1')
+        self.assertEqual(conn.unlocked_password, 'Hello@123')
 
 
 if __name__ == '__main__':
