572e781cc456abd35d1cc8c5b55eaa6b47bb60d5
Tianhua Han <tiahua_han@yahoo.com>
2020-02-09 11:40:50 -0500

    update

--- python3/scripts/selenium_test.py
+++ python3/scripts/selenium_test.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-# https://chromedriver.chromium.org/getting-started
-
-
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -13,28 +10,62 @@ import textwrap
 from pprint import pprint, pformat
 import os
 import io
+import signal
 
 prog = os.path.basename(sys.argv[0])
 
-usage = textwrap.dedent("""
+driver_log = "/tmp/selenium_chromedriver.log"
+
+usage = textwrap.dedent(f"""
     run selenium test
+    
+    +----------+      +--------------+     +----------------+
+    | selenium +----->+ chromedriver +---->+ chrome browser +---->internet
+    +----------+      +--------------+     +----------------+
+    
+    selenium will always start a chromedriver locally.
+    
+    {prog} auto
+    selenium will also start a local browser.
+
+    {prog} host:port
+    selenium will connect to an existing running browser, local or remote.
+    
     """)
 
 examples = textwrap.dedent(f""" 
 examples:
-    - let Selenium::Chrome to start a local browser automatically
-   {prog} auto
+    - let selenium to start a local browser automatically
+    {prog} auto
+    {prog} -ba proxy-pac-url=http://pac.abc.net' auto
 
     - start Chrome (c1) on localhost with debug port 9222.
     /usr/bin/chromium-browser --no-sandbox --disable-dev-shm-usage --window-size=960,540 \
     --user-data-dir=/tmp/selenium_chrome_browser_dir --remote-debugging-port=9222 
-   {prog} local_browser
+   {prog} localhost:9222
 
    - start Chrome (c1) on remote PC with debug port 9222.
+
+    +------------------+       +---------------------+
+    | +---------+      |       |  +---------------+  |
+    | |selenium |      |       |  |chrome browser +------->internet
+    | +---+-----+      |       |  +----+----------+  |
+    |     |            |       |       ^             |
+    |     |            |       |       |             |
+    |     v            |       |       |             |
+    | +---+---------+  |       |  +----+---+         |
+    | |chromedriver +------------>+netpipe |         |
+    | +-------------+  |       |  +--------+         |
+    |                  |       |                     |
+    |                  |       |                     |
+    |  Linux           |       |   PC                |
+    |                  |       |                     |
+    +------------------+       +---------------------+
+
     PC> "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" \
-    --remote-debugging-port=9333 --user-data-dir=%USERPROFILE%\\ChromeTest
-    on the same remote PC, launch cygwin, in cygwin term: netpipe 9222 localhost:9333
-   {prog} remote_browser
+    --remote-debugging-port=9222 --user-data-dir=%USERPROFILE%\\ChromeTest
+    on the same remote PC, launch cygwin, in cygwin term: netpipe 9333 localhost:9222
+   {prog} 192.168.1.164:9333
 
     """)
 
@@ -45,11 +76,6 @@ parser = argparse.ArgumentParser(
     epilog=examples)
 
 parser.add_argument(
-    'test', default=None,
-    choices=['auto', 'local_browser', 'remote_browser'],
-    help='specify the test')
-
-parser.add_argument(
     '--headless', action='store_true', default=False,
     help="headless")
 
@@ -57,17 +83,18 @@ parser.add_argument(
     '-v', '--verbose', default=0, action="count",
     help='verbose level: -v, -vv, -vvv')
 
-# parser.add_argument(
-#     '-s', '--StringInput', action="store_true",
-#     help='input is a string on command line')
-#
-# parser.add_argument(
-#     '-d', '-delimiter', dest="FixDelimiter", default=None, action='store',
-#     help="input FIX delimiter, default to ','")
-#
-# parser.add_argument(
-#     '-dict', dest="FixDict", default=None, action='store',
-#     help="extra dictionary to resolve fix tags and values")
+parser.add_argument(
+    'host_port', default=None,
+    help="host:port of an browser. use 'auto' to let this script to start a browser")
+
+parser.add_argument(
+    '-ba', '--browserArgs', action="append",
+    help='extra args to browser command, eg, -ba proxy-pac-url=http://pac.abc.net. can set multiple times.')
+
+parser.add_argument(
+    '-driver', dest="driver", default="chromedriver", action='store',
+    help="driver, eg, 'chromedriver78', default 'chromedriver', must be in PATH. we use this in case chromedriver and "
+         "chrome browser's version mismatch.")
 
 args = vars(parser.parse_args())
 
@@ -76,76 +103,95 @@ if args['verbose']:
     sys.stderr.write(pformat(args) + "\n")
 
 headless = args['headless']
+driver = args['driver']
 
-chromedriver_service_args = ["--verbose", "--log-path=/tmp/chromedriver.log"]  # for chromedriver
+if args['verbose']:
+    cmd = 'ps -ef|grep chromedriver|grep -v grep'
+    print(cmd)
+    os.system(cmd)
+
+driver_args = ["--verbose", f"--log-path={driver_log}"]  # for chromedriver
+
+cmd = f"cat /dev/null {driver_log}"
+print(cmd)
+os.system(cmd)
+
+# --pid PID  exits when PID is gone
+# -F         retry file if it doesn't exist
+cmd = f"tail --pid {os.getpid()} -F -f {driver_log} &"
+print(cmd)
+os.system(cmd)
 
 # chrome_options will be used on chrome browser's command line not chromedriver's commandline
-chrome_options = Options()
+browser_options = Options()
 
-if args['test'] == 'auto':
-    # we will start up a local chromedriver, which will in turn start a local chrome browser.
-
+if args['host_port'] == 'auto':
+    print('we will start a chromedriver which will start a browser')
     if headless:
-        chrome_options.add_argument("--headless")
+        browser_options.add_argument("--headless")
 
-    chrome_options.add_argument('--no-sandbox')  # to be able to run without root
-    chrome_options.add_argument('--disable-dev_shm-usage')  # to be able to run without root
-    chrome_options.add_argument('-window-size=960,540 --user-data-dir=/tmp/selenium_chrome_browser_dir')
-    # chrome_options.add_argument('--proxy-pac-url=http://pac.abc.net')  # to run with proxy
+    browser_options.add_argument('--no-sandbox')  # to be able to run without root
+    browser_options.add_argument('--disable-dev_shm-usage')  # to be able to run without root
+    browser_options.add_argument('--window-size=960,540 --user-data-dir=/tmp/selenium_chrome_browser_dir')
 
-elif args['test'] == 'local_browser':
-    # we will start local chromedriver which will connect to an existing local chrome browser
+    if args['browserArgs']:
+        for arg in args['browserArgs']:
+            browser_options.add_argument(f'--{arg}')
+            # chrome_options.add_argument('--proxy-pac-url=http://pac.abc.net')  # to run with proxy
+else:
+    print(f'we will start a chrome driver which will connect to an existing browser at host:port={args["host_port"]}')
+    browser_options.debugger_address = args["host_port"]
 
-    print('we will use local browser')
-
-    # this will tell chromedriver to connect a remote browser, not starting one locally
-    chrome_options.debugger_address = 'localhost:9222'
-
-    # chromedriver and chrome browser version must match
+    # chromedriver and chrome browser version would better match
+    #
+    # to check chromedriver version
     # $ /usr/bin/chromedriver -v
     # ChromeDriver 79.0.3945.79 (29f75ce3f42b007bd80361b0dfcfee3a13ff90b8-refs/branch-heads/3945@{#916})
-    # on PC, open Chrome, Help->About Chrome, I see version 79.0....
-    # Matched !!!
+    #
+    # to check browser version
+    # on PC or Linux, open Chrome, Help->About Chrome, I see version 79.0....
+    # on Linux only, chromium-browser --version
 
-    # start the local chrome browser
+    # 1. test with a local browser
+    # start the local chrome browser on linux
     # /usr/bin/chromium-browser --no-sandbox --disable-dev-shm-usage --window-size=960,540 \
     # --user-data-dir=/tmp/selenium_chrome_browser_dir --remote-debugging-port=9222
 
-elif args['test'] == 'remote_browser':
-    # we will start a chromedriver which will connect to an existing chrome browser on a remote PC
-
-    # this will tell chromedriver to connect a remote browser, not starting one locally
-    chrome_options.debugger_address = '192.168.1.164:9333'
-
-    # chromedriver and chrome browser version must match
-    # $ /usr/bin/chromedriver -v
-    # ChromeDriver 79.0.3945.79 (29f75ce3f42b007bd80361b0dfcfee3a13ff90b8-refs/branch-heads/3945@{#916})
-    # on PC, open Chrome, Help->About Chrome, I see version 79.0....
-    # Matched !!!
-
+    # 2. test with a remote browser
     # start Chrome (c1) on PC with debug port 9222.
     # "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir=%USERPROFILE%\ChromeTest
-
+    #
     # on the same pc, start another chrome (c2) in normal way. In chrome c2, enter http://localhost:9222
-    # you should see "inspectable page". click the first link, it will bring up devTools.
+    # you should see "inspected page". click the first link, it will bring up devTools.
     # Then in chrome c1, when go to an url, eg, google.com, chrome c2 will reflect this too
-
+    #
     # chrome c1 can only accept local connection; cannot accept remote connection. Therefore, on the pc, start:
     #    netpipe 9333 localhost:9222
     # then have chromedriver pointing to pc_address:9333
 
-driver = webdriver.Chrome('chromedriver',  # make sure chromedriver is in the PATH
-                          options=chrome_options,  # for chrome browser
-                          service_args=chromedriver_service_args,  # for chromedriver
+driver = webdriver.Chrome(driver,  # make sure chromedriver is in the PATH
+                          options=browser_options,  # for chrome browser
+                          service_args=driver_args,  # for chromedriver
                           )
 
-print(f'driver.title={driver.title}')
+# os.system(f"tail -f {chromedriver_log} &")
+#tail_pid = os.spawnl(os.P_NOWAIT, "/usr/bin/tail", "-f", f"{driver_log}")
+
+# tail_pid = os.fork()
+#
+# if tail_pid == 0:
+#     # child
+#     os.execlp("/usr/bin/tail", "-f", f"{driver_log}")
+
+time.sleep(2)  # give 1 sec to let the tail set up
+
+# print(f'driver.title={driver.title}')
 
 url = 'http://www.google.com/'
 driver.get(url)
 
 if not headless:
-    time.sleep(5)  # Let the user actually see something!
+    time.sleep(2)  # Let the user actually see something!
 
 try:
     search_box = driver.find_element_by_name('q')
@@ -155,5 +201,15 @@ else:
     search_box.send_keys('ChromeDriver')
     search_box.submit()
     if not headless:
-        time.sleep(5)  # Let the user actually see something!
+        time.sleep(2)  # Let the user actually see something!   `
+
 driver.quit()
+
+# if tail_pid:
+#     try:
+#         os.killpg(tail_pid, signal.SIGTERM)
+#     except OSError:
+#         pass
+
+# list all the log files for debug purpose
+os.system("ls -ld /tmp/selenium_*")
