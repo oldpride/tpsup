f2c60dffcbdfcd8b93d7f425bbc805b8fd0e4f18
Tianhua Han <tiahua_han@yahoo.com>
2020-02-10 23:12:59 -0500

    update

--- lib/perl/TPSUP/XML.pm
+++ lib/perl/TPSUP/XML.pm
@@ -5,6 +5,7 @@ use base qw( Exporter );
 our @EXPORT_OK = qw(
    query_xml
    explore_xml
+   parse_inline_xml_log
 );
 
 use Carp;
@@ -16,6 +17,8 @@ use TPSUP::UTIL qw(
    transpose_arrays
    compile_perl_array
 );
+
+use YAML;
 
 #use TPSUP::CSV qw(render_csv);
 sub query_xml {
@@ -151,5 +154,124 @@ sub explore_xml {
    
    return $ret;
 }
+
+# log contains one-line xml message, eg, MQ log
+sub parse_inline_xml_log {
+   my ($file, $opt) = @_;
+
+   croak "$file is not found" if ! -f $file;
+      
+   my $ForceArray;
+      
+   if ($opt->{ForceArray}) {
+      my $type = ref $opt->{ForceArray};
+      
+      if ($type eq 'ARRAY') {
+         @$ForceArray = @{$opt->{ForceArray}};
+      } else {
+         @$ForceArray = split /,/, $opt->{ForceArray};
+      }
+   } else {
+      $ForceArray = [];
+   }
+      
+   my $KeyAttr;
+
+   if ($opt->{KeyAttr}) {
+      my $type = ref $opt->{KeyAttr};
+
+      if ($type eq 'ARRAY') {
+         for my $pair (@{$opt->{KeyAttr}}) {
+            if ($pair =~ /^(.+?)=(.+)/) {
+               $KeyAttr->{$1} = $2;
+            } else {
+               croak "KeyAttr '$pair' in not in format of key=value";
+            }
+         }
+      } elsif ($type eq 'HASH') {
+         $KeyAttr = $opt->{KeyAttr};
+      } else {
+         croak "KeyAttr has unsupport ref type='$type'. only ref to ARRAY or HASH are supported.";
+      }
+   } else {
+      $KeyAttr = [];
+   }
+      
+   if ($opt->{verbose}) {
+      print "ForceArray=", Dumper($ForceArray), "\n";
+      print    "KeyAttr=", Dumper($KeyAttr), "\n";
+   }
+      
+   # noattr => 1 is to keep newline chars, otherwise, they would be converted into spaces
+   # got clue from http://www.perlmonks.org/bare/index.pl/?node_id=249572
+   # but this doesn't work
+   # my $root = XMLin($file, ForceArray=>$ForceArray, KeyAttr=>$KeyAttr, noattr => 1);
+   # neither the following works
+   # http://stackoverflow.com/questions/1457333/php-simplexml-doesnt-preserve-line-breaks-in-xml-attributes
+      
+   my $warn = $opt->{verbose} ? 'use' : 'no';
+   #TPSUP::Expression::export(%$ref);
+      
+   my @compiled_paths;
+
+   for my $p (@{$opt->{paths}}) {
+      #my $compiled = eval "$warn warnings; no strict; package TPSUP::Expression; sub { $p } ";
+      #my $value = eval "$warn; no strict; $p";
+      #my $value = eval $p;
+      my $compiled = eval "$warn warnings; no strict; sub { $p } ";
+      croak "Bad expression '$p': $@" if $@;
+      push @compiled_paths, $compiled;
+   }
+
+   my $match_pattern;
+   if ($opt->{MatchPattern}) {
+      $match_pattern = qr/$opt->{MatchPattern}/;
+   }
+
+   my $exclude_pattern;
+   if ($opt->{ExcludePattern}) {
+      $exclude_pattern = qr/$opt->{ExcludePattern}/;
+   }
+    
+   open my $fh, "<$file" or croak "cannot read $file: $!";
+
+   while(<$fh>) {
+      next if   $match_pattern && ! /$match_pattern/;
+      next if $exclude_pattern && /$exclude_pattern/;
+
+      if (/^.*?<[?]xml.*?>\s*(<.*>)/) {
+         my $string;
+
+         if (defined $opt->{AddRootNode}) {
+            $string = "<$opt->{AddRootNode}>$1</$opt->{AddRootNode}>\n";
+         } else {
+            $string = "$1\n";
+         }
+
+         #print $string;
+      
+         my $root = XMLin($string, ForceArray=>$ForceArray, KeyAttr=>$KeyAttr);
+      
+         $opt->{verbose} && print "root = ", Dumper($root);
+
+         my $ret;
+
+         if ($opt->{paths} && @{$opt->{paths}}) {
+            for my $compiled (@compiled_paths) {
+               push @$ret, $compiled->();
+            }
+         } else {
+            $ret = $root;
+         }
+
+         $opt->{verbose} && print "ret = ", Dumper($ret);
+
+         if ($opt->{yamlOutput}) {
+            print YAML::Dump($ret);
+         }
+      }
+   }
+}
+
    
 1
