7501c5a82998bd378f6692db298889fcf41b1478
Tianhua Han <tianhua_han@yahoo.com>
2020-06-21 18:58:59 +0100

    update

---
+++ cmd_exe/p3env.cmd
@@ -0,0 +1,10 @@
+@REM is for comment
+@REM @ is to turn off echo
+@REM echo. is to print a new line
+
+@rem @set PYTHONPATH=%PYTHONPATH%;C:/users/%USERNAME%/github/tpsup/python3/lib
+@set PYTHONPATH=;C:/users/%USERNAME%/github/tpsup/python3/lib
+@echo PYTHONPATH=%PYTHONPATH%
+@echo.
+@echo Python is
+@where python
