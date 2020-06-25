f7744bbe7775fbf3b294bbfa28f107bb55d36ad3
Tianhua Han <tianhua_han@yahoo.com>
2020-06-25 12:58:00 +0100

    update

--- cmd_exe/tpsup.cmd
+++ cmd_exe/tpsup.cmd
@@ -2,3 +2,5 @@
 
 rem https://superuser.com/questions/129969/navigate-to-previous-directory-in-windows-command-prompt
 doskey cd=mycd $*
+
+p3env
---
+++ python3/behave/examples/features/environment.py
@@ -0,0 +1,11 @@
+import behave_webdriver
+#from behave_webdriver import context
+
+def before_all(context):
+    context.behave_driver = behave_webdriver.Chrome()
+    # for headless
+    #context.behave_driver = behave_webdriver.Chrome().headless()
+
+def after_all(context):
+    # cleanup after tests run
+    context.behave_driver.quit()
