e726743983e5a38b0d7849e3e8e247f62aa6ef3b
Tianhua Han <tianhua_han@yahoo.com>
2020-07-18 00:35:30 -0400

    update

--- ps1/tar.ps1
+++ ps1/tar.ps1
@@ -15,10 +15,8 @@ if ($v) {
 }
 
 $version = "0.0"
-
 Write-Verbose "version = $version"
 
-#$AsciiEncoding = new-object System.Text.AsciiEncoding
 $HeadSize = 512   # tar head size
 $BufferSize = 16*1024
 $buffer = new-object System.Byte[] $BufferSize
@@ -73,74 +71,77 @@ Usage:
    exit 1
 }
 
-# mimic perl implementation
+# only mimiced features of perl implementation of tar, Archive::Tar
 # /usr/share/perl5/5.30/Archive/Tar.pm
 # /usr/share/perl5/5.30/Archive/Tar/File.pm
 
-Class TarConstant {
-   static [Int] $HEAD = 512
+Class Tar {
    # https://www.sans.org/blog/powershell-byte-array-and-hex-functions/
    # in Powershell, Byte and System.Byte are the same.
-   static [Byte[]]$HeadBlockEnd = 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0
-   
-}
-
-Class Tar {
-   [Byte []]$_data = $null
-   [String]$_error = ''
-   [String]$_file = 'Unknown' 
-   [Int] $_bufferSize = 4*1024*1024
-   [System.Byte[]]$_buffer= $null
-   [Int] $_HEAD = 512
-   [Int] $_BLOCK = 512
-   [System.Byte[]]$_endBlock = $null
-   [System.Byte[]]$_headEnd = $null
+   # this marks the end of tar file. It is an empty block.
+   # As it has 512 bytes, hardcode doesn't look nice, therefore will init later
+   #[Byte[]]$EndBlock = $0x0,0x0,.... repeat 512 times
+   [Byte[]]$EndBlock = $null 
+   [String]$file = 'Unknown'  
+   [Byte[]]$Buffer= $null
+   [Int] $HeadSize = 512
+   [Int] $BlockSize = 512
+   [Int] $BufferSize = 1024*512
+   [bigInt] $offset = 0
 
    Tar([String]$file){
-      $this._file = $file
-      $this._buffer = new-object System.Byte[] $this._bufferSize
-      $this._endBlock = new-object System.Byte[] $this._HEAD    # tar end block is an empty block
-      $this._headEnd = new-object System.Byte[] 12   # tar HEAD end block with 12 empty bytes
+      $this.file = $file
+      $this.Buffer = new-object System.Byte[] $this.BufferSize
+      $this.EndBlock = new-object System.Byte[] $this.BlockSize    # tar end block is an empty block
    }
 
-   [object []]read() {
-      # $data = New-Object 'System.Collections.Generic.List[object]'
-      $data = @()
-      #$HEAD = $this._HEAD
-      $HEAD = [TarConstant]::HEAD
- 
+   [object []]read([String]$action) {     
       $handle = $null
-      try { $handle = [System.IO.File]::OpenRead($this._file) } catch { Write-Host $_; exit 1 } 
+      try { $handle = [System.IO.File]::OpenRead($this.file) } catch { Write-Host $_; return $null } 
 
-      [bigInt] $offset = 0
+      $this.offset = 0
 
       #Set-PsDebug -Trace 1
-      while ( $n = $handle.Read($this._buffer, 0, $HEAD)) {
+      while ( $n = $handle.Read($this.Buffer, 0, $this.HeadSize)) {
          Write-Verbose "read $n bytes"
 
-         if ($n -ne $HEAD) {
-            Write-Host "Cannot read enough bytes from the tarfile: imcomplete HEAD at offset=$([String]::Format('0x{0:x}', $offset))"
+         if ($n -ne $this.HeadSize) {
+            Write-Host "Cannot read enough bytes from the tarfile: imcomplete HEAD at offset=$([String]::Format('0x{0:x}', $this.offset))"
             return $null
          } 
 
-         if ($this.compare_array($this._buffer[0..511], $this._endBlock)) {
+         $BlockEndIndex = $this.HeadSize-1
+         if ([Tar]::compare_array($this.Buffer[0..$BlockEndIndex], $this.EndBlock)) {
             # a empty block means tar end
-            Write-Verbose "hit ending empty block at offset=$([String]::Format('0x{0:x}', $offset))"
+            Write-Verbose "hit ending empty block at offset=$([String]::Format('0x{0:x}', $this.offset))"
             break
          }       
 
          [hashtable] $entry = $this.parse_chunk()        
          Write-Verbose "entry = $(ConvertTo-Json($entry))"
-         Write-Verbose "type = $($this.get_type($entry['type']))"
+
+         $this.offset += $n
 
          if (!$entry) {
-            Write-Host "Couldn't read HEAD chunk at offset=$([String]::Format('0x{0:x}', $offset))"
-            next
+            Write-Host "Couldn't read HEAD chunk at offset=$([String]::Format('0x{0:x}', $this.offset))"a
+            continue
+         }        
+
+         $entry_type = $this.get_type_desc($entry['type'])
+
+         $mode1=$null
+
+         if ($entry_type -eq 'DIR' ) {
+            $mode1="d"
+         } else {
+            $mode1="-"
          }
 
-         $offset += $n
+         Write-Host( "{0,-1}{1,-4}{2,-17}{3,-15}`n" -f $mode1, [Convert]::ToString($entry['mode'],8), 2, 1) #"$entry['uid']/$entry['gid']", 1
 
-         $entry_type = $this.get_type($entry['type'])
+
+
+
 
          if ($entry_type -eq 'LABEL') {
             # skip labels
@@ -148,55 +149,40 @@ Class Tar {
          }
 
          if ($entry_type -eq 'FILE' -or $entry_type -eq 'LONGLINK') {
-            $block = $this.BLOCK_SIZE($entry['size'])          
+            $need_size = $this.compute_need_size($entry['size'])          
             
+            [bool] $skip = 0
             if ( $entry['name'] -eq 'pax_global_header' -or $entry['type'] -match '^(x|g)$' ) {
                # skip PAX header
-               $this_block = 64 * 512
-               $amt = $block
-               while ($amt -gt 0) {
-                  if ($this_block -gt $amt) {
-                     $this_block = $amt
-                  }
-
-                  $n2 = $handle.Read($this._buffer, 0, $this_block)
-                  if ($n2 -lt $this_block) {
-                     Write-Host "Read error on tarfile (missing data) at offset=$([String]::Format('0x{0:x}', $offset)). Expected $this_block vs actual $n2 "
-                     return $null
-                  }
-
-                  $offset += $this_block
-                  $amt    -= $this_block
-               }           
-            } else {
-               $this_block = 64 * 512
-               $amt = $block
-               while ($amt -gt 0) {
-                  if ($this_block -gt $amt) {
-                     $this_block = $amt
-                  }
-
-                  $n2 = $handle.Read($this._buffer, 0, $this_block)
-                  if ($n2 -lt $this_block) {
-                     Write-Host "Read error on tarfile (missing data) at offset=$([String]::Format('0x{0:x}', $offset)). Expected $this_block vs actual $n2 "
-                     return $null
-                  }
-
-                  $offset += $this_block
-                  $amt    -= $this_block
-               }
+               $skip = 1
             }
+
+            $attempt_size = $this.BufferSize
+            $amount_left = $need_size
+            while ($amount_left -gt 0) {
+               if ($attempt_size -gt $amount_left) {
+                   $attempt_size = $amount_left
+               }
+
+               $n2 = $handle.Read($this.Buffer, 0, $attempt_size)
+               if ($n2 -lt $attempt_size) {
+                  Write-Host "Read error on tarfile (missing data) at offset=$([String]::Format('0x{0:x}', $this.offset)). Expected $attempt_size vs actual $n2 "
+                  return $null
+               }
+
+               $this.offset += $attempt_size
+               $amount_left -= $attempt_size
+            }                      
          }
       }
    
       #Set-PsDebug -Trace 0
       $handle.close()
       
-      return $data
+      return $null
    }
 
-   <#
-   [bool]compare_array([object[]]$a1, [object[]]$a2) {
+   static [bool] compare_array([byte[]]$a1, [byte[]]$a2) {
       # https://stackoverflow.com/questions/9598173/comparing-array-variables-in-powershell
       # $areEqual = @(Compare-Object $firstFolder $secondFolder -SyncWindow 0).Length -eq 0
       # This will return an array of differences between the two arrays or $null when the
@@ -204,41 +190,22 @@ Class Tar {
       # each item that exists only in one array and not the other. The -SyncWindow 0 
       # argument will make the order in which the items appear in the arrays count as a
       # difference.
-      $areEqual = @(Compare-Object $a1 $a2 -SyncWindow 0).Length -eq 0
-      return $areEqual
-
-   }
-   #>
-
-   [bool]compare_array([byte[]]$a1, [byte[]]$a2) {
       $areEqual =@(Compare-Object $a1 $a2 -SyncWindow 0).Length -eq 0
       return $areEqual
-      <#
-      if ($a1.Length -ne $a2.Length ) {
-         return $false
-      }
-
-      for ($i=0; $i -lt $a1.Length; $i++) {
-         if ($a1[$i] -ne $a2[$i]) {
-            return $false
-         }
-      }
-
-      return $true
-      #>
    }
 
    [object] parse_chunk () {
-
       ### according to the posix spec, the last 12 bytes of the header are
       ### null bytes, to pad it to a 512 byte block. That means if these
       ### bytes are NOT null bytes, it's a corrupt header. See:
       ### www.koders.com/c/fidCE473AD3D9F835D690259D60AD5654591D91D5BA.aspx
       ### line 111
-      # todo: optimize the code
+      [Byte[]]$HeadBlockEnd = 0x0,0x0,0x0,0x0,
+                              0x0,0x0,0x0,0x0,
+                              0x0,0x0,0x0,0x0
 
-      if (! $this.compare_array($this._buffer[-12..-1], $this._headEnd)) {
-         Write-Host  "Invalid header block, ending 12 bytes should be empty \\0"
+      if (! [Tar]::compare_array($this.Buffer[-12..-1], $HeadBlockEnd)) {
+         Write-Host  "Invalid head block starting at offset=$([String]::Format('0x{0:x}', $this.offset)). This block's ending 12 bytes should be empty \\0"
          return $null
       } 
 
@@ -250,7 +217,7 @@ Class Tar {
         mode        = @{ octal=1; size=8  }
         uid         = @{ octal=1; size=8  }
         gid         = @{ octal=1; size=8  }
-        size        = @{ octal=0; size=12 } # cdrake - not *always* octal..
+        size        = @{ octal=0; size=12 } # not *always* octal..
         mtime       = @{ octal=1; size=12 }
         chksum      = @{ octal=1; size=8  }
         type        = @{ octal=0; size=1; } # character
@@ -259,8 +226,8 @@ Class Tar {
         version     = @{ octal=0; size=2  } # 2 bytes
         uname       = @{ octal=0; size=32 } # string                       
         gname       = @{ octal=0; size=32 } # string                        
-        devmajor    = @{ octal=1; size=8             } 
-        devminor    = @{ octal=1; size=8             }
+        devmajor    = @{ octal=1; size=8  } 
+        devminor    = @{ octal=1; size=8  }
         prefix      = @{ octal=0; size=155*12; numeric=0 } # 155 x 12
       }
 
@@ -269,21 +236,21 @@ Class Tar {
 
       [hashtable] $entry = @{}
 
-      $offset = 0
+      $relative_offset = 0
       foreach ($f in $handled_fields) {
          $size = $tmpl[$f]['size']
          $octal = $tmpl[$f]['octal']
-         #$bytes = $this._buffer[$offset, $offset + $size]
-         [String] $string = [System.Text.Encoding]::ASCII.GetString($this._buffer, $offset, $size)
-         #$string = $string.Trim([char]0) # this only trims the ending zeros
+         [String] $string = [System.Text.Encoding]::ASCII.GetString($this.Buffer, $relative_offset, $size)
+
+         # this only trims the ending 0x0es. 
+         #$string = $string.Trim([char]0) 
+
+         # we want to chop everything starting from the first 0x0
          $index = $string.IndexOf([char]0);
-         if ($index -ge 0) { $string = $string.Remove($index) }
-         
-         $length = $string.Length   
+         if ($index -ge 0) { $string = $string.Remove($index) } 
 
-         Write-Verbose "field=$f, offset=$([String]::Format('0x{0:x}', $offset)), size=$size, octal=$octal, string=$string, length=$length"
+         Write-Verbose "field=$f, rel_offset=$([String]::Format('0x{0:x}', $relative_offset)), size=$size, octal=$octal, string=$string, length=$($string.Length)"
 
-         #continue
          if ($f -eq 'size') {
             # todo: should convert to bigint
             $entry[$f] = [Convert]::ToInt64($string,8)
@@ -294,14 +261,14 @@ Class Tar {
             $entry[$f] = $string
          }     
          
-         $offset += $size   
+         $relative_offset += $size   
       }
 
       return $entry
    }
 
-   [string] get_type ([string] $char) {
-      [hashtable] $type_by_char = @{
+   [string] get_type_desc ([string] $char) {
+      [hashtable] $desc_by_char = @{
          "0"="FILE"
          "1"="HARDLINK"
          "2"="SYMLINK"
@@ -315,15 +282,15 @@ Class Tar {
          "V"="LABEL"
       }
 
-      if ($type_by_char.Contains($char)) {
-        return $type_by_char[$char]
+      if ($desc_by_char.Contains($char)) {
+        return $desc_by_char[$char]
       } else {
         return "UNKNOWN"
       }
    }
 
-   [string] get_typechar ([string] $type) {
-      [hashtable] $char_by_type = @{
+   [string] get_type_char ([string] $desc) {
+      [hashtable] $char_by_desc = @{
          "FILE"="0"
          "HARDLINK"="1"
          "SYMLINK"="2"
@@ -337,31 +304,26 @@ Class Tar {
          "LABEL"="V"
       }
 
-      if ($char_by_type.Contains($type)) {
-        return $char_by_type[$type]
+      if ($char_by_desc.Contains($desc)) {
+        return $char_by_desc[$desc]
       } else {
         return "UNKNOWN"
       }
    }
 
-   [Int64] BLOCK_SIZE ([Int64] $size) {
-      [Int64] $n = $size/$this._BLOCK
-      if ($size % $this._BLOCK) {
+   [Int64] compute_need_size ([Int64] $size) {
+      [Int64] $n = $size/$this.BlockSize
+      if ($size % $this.BlockSize) {
          $n += 1
       }
-
-      return $n*$this._BLOCK
+      return $n*$this.BlockSize
    }
-
 }
-
 
 
 if ($file -eq 'unknown') {
       usage("wrong numnber of args")
 }
-
-$abs_pattern = '^[a-zA-Z]:[\\/]|^[\\/]+|^[\\/]+cygdrive[\\/]+[^\\/]+[\\/]'
 
 if ($file) {
    if ( ! ($file -match $abs_pattern) ) {
@@ -370,6 +332,6 @@ if ($file) {
    Write-host "file=$file"
 }
 $tar = [Tar]::new($file)
-$tar.read()
+$tar.read('list')
 
 exit 0
