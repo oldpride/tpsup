26cd09279dc484b96a754f79b3d877dbc1761af6
Tianhua Han <tianhua_han@yahoo.com>
2020-07-28 00:52:57 +0100

    update

--- cmd_exe/p3env.cmd
+++ cmd_exe/p3env.cmd
@@ -3,7 +3,7 @@
 @REM echo. is to print a new line
 
 @rem @set PYTHONPATH=%PYTHONPATH%;C:/users/%USERNAME%/github/tpsup/python3/lib
-@set PYTHONPATH=;C:/users/%USERNAME%/github/tpsup/python3/lib
+@set PYTHONPATH=;C:/users/%USERNAME%/sitebase/github/tpsup/python3/lib
 @echo PYTHONPATH=%PYTHONPATH%
 @echo.
 @echo Python is
--- cmd_exe/siteenv.cmd
+++ cmd_exe/siteenv.cmd
@@ -2,6 +2,12 @@
 
 rem: copy this file to %USERPROFILE%, that is, C:/Users/%USERNAME%
 
-set PATH=%PATH%;%userprofile%/site-support/github/tpsup/cmd_exe
+rem: 1. don't use setx
+rem:    setx vs set
+rem:       setx var value     permanently change
+rem:       set  var=value     only change the current shell
+rem: 2. don't use quotes, for example, below. this would make one big part 
+rem:       set PATH="%PATH:%userprofile%/site..."
+set PATH=%PATH%;%userprofile%/sitebase/github/tpsup/cmd_exe
 
 tpsup
