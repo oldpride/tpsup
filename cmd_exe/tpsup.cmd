ea9e6914d6122b5a918e3d64ec1fa0d53f444301
Tianhua Han <tianhua_han@yahoo.com>
2020-07-28 19:17:10 +0100

    update

--- cmd_exe/tpsup.cmd
+++ cmd_exe/tpsup.cmd
@@ -1,5 +1,14 @@
 @echo off
 
+rem use 'call' to invoke external script; otherwise, the current script will exit after the external script
+call %userprofile%/sitebase/github/tpsup/cmd_exe/addpath.cmd %userprofile%/sitebase/github/tpsup/cmd_exe
+
+rem blank line
+echo. 
+echo path=%path%
+
+echo. 
+
 rem https://superuser.com/questions/129969/navigate-to-previous-directory-in-windows-command-prompt
 doskey cd=mycd $*
 
