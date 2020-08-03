90bd1b3e1360889cbd6cfe76db26e40de5bfa2cb
Tianhua Han <tianhua_han@yahoo.com>
2020-08-03 12:21:28 -0400

    update

--- python3/lib/tpsup/nettools.py
+++ python3/lib/tpsup/nettools.py
@@ -1,6 +1,8 @@
+import functools
 import socket
 import sys
 import time
+from pprint import pformat
 
 import tpsup.coder
 
@@ -14,9 +16,48 @@ def is_tcp_open(_host: str, _port: str):
     else:
         return False
 
+# abandoned the design to subclass socket.socket
+# reason:
+#     1. for server, we need to init the coder after accept() created client socket, not at __init__().
+#        because each client socket needs to have its own coder (specifically code index).
+#     2. for client, not that big deal as client only has one server, but it still looks better to
+#        set up the coder after connect() succeeds.
+#
+# Using inheritance was for the following reason:
+#
+# class TpSocket(socket.socket):
+#     """a wrapper socket to provide encryption
+#
+#         I first tried to replace the instance method like below,
+#             def sendall(data: bytes, flags: int = 0) -> None:
+#                 return sock.socket.sendall(out_coder.xor(data), flags)
+#             def recv(size: int, flags: int = 0) -> bytes:
+#                 return in_coder.xor(sock.socket.recv(size, flags))
+#             sock.sendall = sendall
+#             sock.recv = recv
+#         but got error:
+#             AttributeError: 'socket' object attribute 'sendall' is read-only
+#         Therefore, i switched to use inheritance
+#     """
+#
+#     def __init__(self, key: str = None, *args):
+#         if key is None or key == '':
+#             key = None
+#         self.key = key
+#         self.in_coder = tpsup.coder.Coder(key)
+#         self.out_coder = tpsup.coder.Coder(key)
+#         super().__init__(*args)
+#     def sendall(self, data: bytes, size: int = -1) -> None:
+#         super().sendall(self.out_coder.xor(data, size=size))
+#     def recv(self, size: int, flags=0) -> bytes:
+#         return self.in_coder.xor(super().recv(size))
 
-class TpConn:
+
+class encryptedsocket:
+    """ encrypt an existing socket"""
     def __init__(self, sock: socket.socket, key: str = None, **opt):
+        if not sock:
+            raise RuntimeError('socket is not initialized yet')
         self.socket = sock
         if key is None or key == '':
             key = None
@@ -25,57 +66,95 @@ class TpConn:
         self.in_coder = tpsup.coder.Coder(key)
         self.out_coder = tpsup.coder.Coder(key)
 
-    def send(self, data: bytes, size: int = -1, **opt) -> int:
-        ''' mimic socket.send(bytes[,flags])
+    def sendall(self, data: bytes, size: int = -1) -> None:
+        ''' mimic socket.sendall(bytes[,flags])
         https://docs.python.org/3/library/socket.html
         '''
-        self.socket.send(self.out_coder.xor(data, size=size))
+        self.socket.sendall(self.out_coder.xor(data, size=size))
+        '''
+        socket.send() vs socket.sendall()
 
-    def sendString(self, string: str) -> int:
-        self.send(string.encode('utf-8'))
+        socket.send(bytes[, flags]) 
+        Send data to the socket. Returns the number of bytes sent. Applications are responsible
+        for checking that all data has been sent; if only some of the data was transmitted, the application
+        needs to attempt delivery of the remaining data.
 
-    def recv(self, size, **opt) -> bytes:
-        '''socket.recv(bufsize[, flags])
+        socket.sendall(bytes[, flags]) 
+        Send data to the socket. Unlike send(), this method continues to 
+        send data from bytes until either all data has been sent or an error occurs. None is returned on success. On 
+        error, an exception is raised, and there is no way to determine how much data, if any, was successfully sent. 
+        '''
+
+    def send_string(self, string: str) -> None:
+        self.sendall(string.encode('utf-8'))
+
+    def recv(self, size: int, flags=0) -> bytes:
+        """socket.recv(bufsize[, flags])
         https://docs.python.org/3/library/socket.html
         size is the max size
-        '''
+        """
         return self.in_coder.xor(self.socket.recv(size))
 
-    def recvString(self, size, **opt) -> str:
-        ''' size is max size'''
+    def recv_string(self, size) -> str:
+        """ size is max size"""
         return self.recv(size).decode('utf-8')
 
     def close(self):
         self.socket.close()
         self.socket = None
+        self.in_coder = None
+        self.out_coder = None
 
     # python socket has no flush()
     #   https: // stackoverflow.com / questions / 4407835 / python - socket - flush
 
 
-def connect(host: str, port: int, **opt) -> TpConn:
-    key = opt.get("encode", None)
-    maxtry = opt.get("maxtry", 5)
-    interval = 3
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        for i in range(0,maxtry):
-            try:
-                sock.connect((host, int(port)))
-                conn = TpConn(sock, key)
-                return conn
-            except Exception as e:
-                print(f'{i+1} try out of {maxtry} failed to connect: {e}', file=sys.stderr)
-                if i+1 < maxtry:
-                    print(f'will retry after {interval}', file=sys.stderr)
-                else:
-                    break
-            time.sleep(interval)
-
-
 def main():
-    connect('localhost', 9999)
+    print(f'sys.args={pformat(sys.argv)}')
+
+    if len(sys.argv) < 2:
+        print("usage: prog client|server")
+        sys.exit(1)
+
+    key="abc"
+    host="localhost"
+    port='3333'
+
+    # https://docs.python.org/3/howto/sockets.html
+
+    if sys.argv[1] == 'client':
+        data = 'hello server'
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((host, int(port)))
+            ensock = encryptedsocket(sock, key)
+            ensock.sendall(bytes(data + "\n", "utf-8"))
+            # Receive data from the server and shut down
+            received = str(ensock.recv(1024), "utf-8")
+
+        print("Sent:     {}".format(data))
+        print("Received: {}".format(received))
+    elif sys.argv[1] == 'server':
+        data = "hello client"
+        # create an INET, STREAMing socket
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # bind the socket to a public host, and a well-known port
+        serversocket.bind(('0.0.0.0',int(port)))  # 0.0.0.0 means all interfaces
+        # become a server socket
+        serversocket.listen(5)
+        while True:
+            # accept connections from outside
+            (clientsocket, address) = serversocket.accept()
+            print(f"accepted client socket {clientsocket}")
+            ensock = encryptedsocket(clientsocket, key)
+            ensock.sendall(bytes(data + "\n", "utf-8"))
+            # Receive data from the server and shut down
+            received = str(ensock.recv(1024), "utf-8")
+
+            print("Sent:     {}".format(data))
+            print("Received: {}".format(received))
+            ensock.close()
+
 
 if __name__ == '__main__':
     main()
-
-
--- python3/lib/tpsup/util.py
+++ python3/lib/tpsup/util.py
@@ -1,10 +1,13 @@
 import functools
 import inspect
+import io
+import os
 import re
 import sys
 import traceback
 import types
 from pprint import pformat
+from time import strftime, gmtime
 from typing import Dict, List
 
 
@@ -124,36 +127,42 @@ def load_module(source: str, new_module_name=None):
     return mod
 
 
-def run_module_file(mod_file: str, **opt):
+def run_module(mod: str, mod_is_file=False, **opt):
     verbose = opt.get('verbose', 0)
 
-    if mod_file is None:
+    if mod is None:
         if verbose:
             print('mod_file is None. skipped', file=sys.stderr)
             if verbose > 1:
                 traceback.print_stack() # default to stderr, set file=sys.stdout for stdout
         return
 
-    with open(mod_file, 'r') as f:
-        source = f.read()
+    if mod_is_file:
+        with open(mod, 'r') as f:
+            source = f.read()
+    else:
+        source = mod
+
+    module = None
+    try:
+        module = load_module(source)
+    except Exception:
+        traceback.print_stack()  # e.printStackTrace equivalent in python
         module = None
-        try:
-            module = load_module(source)
-        except Exception:
-            traceback.print_stack()  # e.printStackTrace equivalent in python
-            module = None
 
-        if module is None:
-            sys.stderr.write(f"failed to compile: {mod_file}\n")
-            return
+    if module is None:
+        sys.stderr.write(f"failed to compile: {mod}\n")
+        return
 
-        if opt.get('dryrun', False):
-            sys.stderr.write(f"dryrun mode: {mod_file} compiled successfully\n")
-        else:
-            sys.stderr.write(f"running: {mod_file}\n")
-            module.run(mod_file=mod_file, **opt)  # pycharm had a warning here. I set to ignore
+    if opt.get('dryrun', False):
+        sys.stderr.write(f"dryrun mode: {mod} compiled successfully\n")
+    else:
+        sys.stderr.write(f"running: {mod}\n")
+        module.run(mod_file=mod, **opt)  # pycharm had a warning here. I set to ignore
+
 
 def silence_BrokenPipeError(func):
+    ''' replace build-in functions'''
     @functools.wraps(func)
     def silenced(*args, **kwargs):
         result = None
@@ -166,8 +175,54 @@ def silence_BrokenPipeError(func):
     return silenced
 
 
+def tplog(message:str= None, file=sys.stderr, **opt):
+    if message is None:
+        message = ''
+    timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+    # print(pformat(caller)) *o
+    # FrameInfo(frame=<frame object at 0x7f0ce4e87af8>, filename='util.py', lineno=176, function='tplog',
+    # code_context=['    caller = inspect.stack()\n'], index=0),
+    # FrameInfo(frame=<frame object at 0x7f0ce4a84048>, filename='util.py', lineno=182, function='main',
+    # code_context=["    tplog('test')\n"], index=0),
+    # FrameInfo(frame=<frame object at 0x12e3428>, filename='util.py', lineno=185, function='<module>',
+    # code_context=['    main()\n'], index=0)]
+    caller = inspect.stack()[1]
+
+    print(f"{timestamp} {os.path.basename(caller.filename)},{caller.lineno},{caller.function} {message}", file=file, **opt)
+
+
+def print_exception(e: Exception, stacktrace=True, **opt):
+    file = opt.get('file', sys.stderr)
+
+    sio = None
+    if file == str:
+        # print to string
+        sio = io.StringIO()
+        file = sio
+    if stacktrace:
+        print(traceback.format_exc(), file=file)
+    else:
+        # print("{0}: {1!r}".format(type(e).__name__, e.args), file=file, **opt)
+        print("{0}: {1}".format(type(e).__name__, ";".join(e.args)), file=file, **opt)
+
+    if sio:
+        string = sio.getvalue()
+        sio.close()
+        return string
+
+def tplog_exception(e: Exception, **opt):
+    tplog(print_exception(e, file=str), **opt)
+
+
 def main():
-    print(f"not done")
+    tplog('hello world')
+
+    try:
+        raise RuntimeError("test exception")
+    except Exception as e:
+        print_exception(e)
+        tplog(print_exception(e, file=str))
 
 
 if __name__ == '__main__':
