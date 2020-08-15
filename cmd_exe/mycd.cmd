920427745e5a48b57e437e34966d5afa2afd6e92
Tianhua Han <tianhua_han@yahoo.com>
2020-08-15 05:20:00 +0100

    update

--- cmd_exe/mycd.cmd
+++ cmd_exe/mycd.cmd
@@ -1,13 +1,20 @@
 @rem https://superuser.com/questions/129969/navigate-to-previous-directory-in-windows-command-prompt
 @echo off
-rem if '%*'=='' cd %userprofile% & exit /b
+@rem if '%*'=='' cd %userprofile% & exit /b
 if '%*'=='' (
     cd /d %userprofile%
     set OLDPWD=%cd%
 ) else if '%*'=='-' (
-    cd /d %OLDPWD%
+    @rem ^ is escape char. When variable %OLDPWD% is undefined, it is literal %OLDPWD%
+    if NOT '%OLDPWD%' == '^%OLDPWD^%' (
+       cd /d %OLDPWD%
+    ) else (
+       echo set OLDPWD=%cd%
+    )
     set OLDPWD=%cd%
 ) else (
     cd /d %*
-    if not errorlevel 1 set OLDPWD=%cd%
+    if not errorlevel 1 (
+       set OLDPWD=%cd%
+    )
 )
