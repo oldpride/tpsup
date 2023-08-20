e904e5ac1dad0b2d2388fff08a30cae0d9be4c80
Tianhua Han <tianhua_han@yahoo.com>
2023-08-20 17:38:52 +0100

    update

--- python3/lib/tpsup/greptools.py
+++ python3/lib/tpsup/greptools.py
@@ -77,18 +77,21 @@ def grep(files: Union[list, str], MatchPattern: str = None,
 
         # skip directories
         if os.path.isdir(file):
-            if file in exclude_dirs:
-                if verbose:
-                    log_FileFuncLine(
-                        f'{file} is in exclude_dirs, skip', file=sys.stderr)
-                continue
             if Recursive:
+                if file in exclude_dirs:
+                    if verbose:
+                        log_FileFuncLine(
+                            f'{file} is in exclude_dirs, skip', file=sys.stderr)
+                    continue
+
                 for root, dirs, fnames in os.walk(file, topdown=True):
                     # https://stackoverflow.com/questions/19859840/excluding-directories-in-os-walk
                     # key point: use [:] to modify dirs in place
                     dirs[:] = [d for d in dirs if d not in exclude_dirs]
                     for f in fnames:
                         full_path = os.path.join(root, f)
+                        if verbose:
+                            print(f'grep {full_path}', file=sys.stderr)
                         matches = grep_1_file(full_path,
                                               MatchCompiled=MatchCompiled,
                                               ExcludeCompiled=ExcludeCompiled,
@@ -96,17 +99,21 @@ def grep(files: Union[list, str], MatchPattern: str = None,
                                               print_filename=print_filename,
                                               **opt)
                         lines.extend(matches)
+            else:
+                if verbose:
+                    print(f'{file} is a directory, skip', file=sys.stderr)
             continue
 
         if verbose:
             print(f'grep {file}', file=sys.stderr)
-            match = grep_1_file(file,
-                                MatchCompiled=MatchCompiled,
-                                ExcludeCompiled=ExcludeCompiled,
-                                FileNameOnly=FileNameOnly,
-                                print_filename=print_filename,
-                                **opt)
-            lines.extend(match)
+
+        match = grep_1_file(file,
+                            MatchCompiled=MatchCompiled,
+                            ExcludeCompiled=ExcludeCompiled,
+                            FileNameOnly=FileNameOnly,
+                            print_filename=print_filename,
+                            **opt)
+        lines.extend(match)
 
     return lines
 
@@ -171,74 +178,6 @@ def grep_1_file(file: str,
     return lines
 
 
-def grepl(files: Union[list, str], MatchPatterns: list, **opt):
-    """
-    extend unix "grep -l" to support multiple patterns.
-    grepl is file-based grep, meaning multiple patterns are in the same file but can be in different lines.
-    """
-    verbose = opt.get('verbose', 0)
-    files2 = []
-    if isinstance(files, str):
-        # split string by space or newline
-        for f in re.split('\s+', files, re.MULTILINE):
-            files2.extend(glob(f))
-    else:
-        for f in files:
-            files2.extend(glob(f))
-
-    files3 = []
-    if len((MatchPatterns)) == 0:
-        raise RuntimeError('MatchPatterns is empty')
-
-    CompiledPatterns = {}
-    for p in MatchPatterns:
-        CompiledPatterns[p] = re.compile(p)
-
-    seen_file = {}
-
-    for file in files2:
-        if seen_file.get('file', False):
-            continue
-        else:
-            seen_file[file] = True
-
-        # skip directories
-        if os.path.isdir(file):
-            print(f'{file} is a directory, skip', file=sys.stderr)
-            continue
-
-        if verbose:
-            print(f'grep {file}', file=sys.stderr)
-
-        c2 = CompiledPatterns.copy()  # copy to avoid changing the original
-
-        with TpInput(filename=file, **opt) as tf:
-            # Regex is built inside TpInput
-
-            try:
-                for line in tf:
-                    (keys, values) = zip(*CompiledPatterns.items())
-                    for i in range(len(keys)):
-                        if values[i].search(line):
-                            c2.pop(keys[i])
-                    # checkif c2 is empty
-                    if len(c2) == 0:
-                        files3.append(file)
-                        if opt.get('print', False):
-                            print(file)
-                        break
-            except UnicodeDecodeError as e:
-                # UnicodeDecodeError: 'utf-8' codec can't decode byte 0x9b in position 147:
-                #     invalid start byte
-                print(
-                    f'grep {file} failed with decode error. skipped.', file=sys.stderr)
-                if verbose:
-                    print(e, file=sys.stderr)
-                continue
-
-    return files3
-
-
 def main():
     import os
     TPSUP = os.environ.get('TPSUP')
@@ -248,7 +187,6 @@ def main():
         grep(files, 'mypattern')
         grep(files, ExcludePattern='abc|def')
         grep(files, 'mypattern', FileNameOnly=True)
-        grepl(files, ['mypattern', 'abc1'])
 
     from tpsup.exectools import test_lines
     test_lines(test_codes, source_globals=globals(), source_locals=locals())
