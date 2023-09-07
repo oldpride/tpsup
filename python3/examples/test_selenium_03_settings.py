eccbf9157bfd09d3c5d9ae12954d93d69f93eff5
Tianhua Han <tianhua_han@yahoo.com>
2023-09-07 23:15:29 +0100

    update

--- python3/examples/test_selenium_03_settings.py
+++ python3/examples/test_selenium_03_settings.py
@@ -9,8 +9,15 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 import tpsup.env
 import os
 
+
 env = tpsup.env.Env()
 home_dir = env.home_dir
+
+
+# # https://www.selenium.dev/documentation/webdriver/troubleshooting/logging/
+# import logging
+# logger = logging.getLogger('selenium')
+# logger.setLevel(logging.DEBUG)  # default is WARN. DEBUG is the most verbose.
 
 options = Options()
 options.page_load_strategy = 'normal'
@@ -55,38 +62,60 @@ else:
     options.binary_location = f"{SITEBASE}/Windows/10.0/Chrome/Application/chrome.exe"
 
 print(f"options.arguments = \n{pformat(options.arguments)}")
+print()
+print(f"options.to_capabilities = \n {pformat(options.to_capabilities())}")
+print()
+print('browser command =')
+print(f"{options.binary_location} {' '.join(options.arguments)}")
 
 driver_args = [
     "--verbose",
+    "--enable-chrome-logs",
+    f"--log-path={home_dir}/selenium_driver2.log",
+
 ]  # for chromedriver
 
 # https://www.selenium.dev/documentation/webdriver/drivers/service/
 # default is to use chromedriver on command line
 # service=ChromeService(ChromeDriverManager().install()) # this is not default service.
 
+# The Service classes are for managing the starting and stopping of local drivers.
 if env.isLinux:
     service = ChromeService(
         executable_path=f"/usr/bin/chromedriver",
-        log_path=f"{home_dir}/selenium_driver.log",
-        driver_args=driver_args,
+        service_args=driver_args,
+
+        # log_path will be deprecated in the future, use log_output instead
+        # see <venv>\Lib\site-packages\selenium\webdriver\chromium\service.py
+        # log_path=f"{home_dir}/selenium_driver.log",
+        # I set it in service_args instead. see above
     )
 else:
     service = ChromeService(
-        # he Service classes are for managing the starting and stopping of local drivers.
         executable_path=f"{SITEBASE}/Windows/10.0/chromedriver/chromedriver.exe",
-        log_path=f"{home_dir}/selenium_driver.log",
-        driver_args=driver_args,
+        service_args=driver_args,
+        # log_path=f"{home_dir}/selenium_driver.log", # commented out because see above
     )
 
+# print(
+#     f"\nservice.command_line_args() = {pformat(service.command_line_args())}\n")
+print()
+print(f"driver_command = ")
+print(f"{service.path} {' '.join(service.command_line_args())}")
+print()
 
 driver = webdriver.Chrome(
     service=service,
-    options=options
+    options=options,
 )
-#driver = webdriver.Chrome()
-driver.get("http://www.google.com")
+# driver = webdriver.Chrome()
+
+url = f"file:///{os.path.normpath(os.environ.get('TPSUP'))}/scripts/tpslnm_test_input.html"
+# driver.get("http://www.google.com")
+driver.get(url)
+
 print(f'Page title was {driver.title}')
 
 # sleep 5 seconds so you can see the page load
-time.sleep(3)
+time.sleep(2)
 driver.quit()
