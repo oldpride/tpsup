e5ee5fc069f780d16027958b4ed30f8c959fb93b
Tianhua Han <tiahua_han@yahoo.com>
2025-09-23 22:08:40 +0100

    update

--- python3/scripts/ptputty_cfg_batch.py
+++ python3/scripts/ptputty_cfg_batch.py
@@ -106,7 +106,7 @@ def code(all_cfg, known, **opt):
         # for user to type the command to login.
         # we loop here until the title changes, meaning the putty window is "matured"
         # and ready for next command.
-        'while=exp=titlevar[0].endwith(" - PuTTY")',
+        'while=exp=titlevar[0].endswith(" - PuTTY")',
         'sleep=2',
         'texts=titlevar',
         'end_while',
