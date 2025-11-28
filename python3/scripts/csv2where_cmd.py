02e47838087357d74d09fdac762b47d990e4712b
Tianhua Han <tiahua_han@yahoo.com>
2025-11-28 17:16:22 +0000

    update

--- python3/scripts/csv2where_cmd.py
+++ python3/scripts/csv2where_cmd.py
@@ -66,7 +66,7 @@ def csv2where(csvfile, sep=',', quotechar='"', add_quotes=True, debug=False):
                     type_hint = m.group(2)
 
                     if type_hint:
-                        if re.search(r'number|int|float|abs', type_hint, re.IGNORECASE):
+                        if re.search(r'number|int|float|abs|neg', type_hint, re.IGNORECASE):
                             add_quotes = False
 
                             # remove ',' from value, eg, '1,234' to '1234'
@@ -79,6 +79,8 @@ def csv2where(csvfile, sep=',', quotechar='"', add_quotes=True, debug=False):
                             if re.search(r'abs', type_hint, re.IGNORECASE):
                                 key = f"abs({key})"
                                 value = abs(float(value))
+                            elif re.search(r'neg', type_hint, re.IGNORECASE):
+                                value = -float(value)
 
                 if add_quotes:
                     conditions.append(f"{key}='{value}'")
